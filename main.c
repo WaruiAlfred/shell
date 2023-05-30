6fbd2fabfe1404e264e8c4c9c580c5bc4b87f9e9
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-30 17:17:21 +0300

    code to read what a user types in

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 
 int main(int ac,char **argv){
   char *prompt = "(shell) $";
+  char *lineptr;
+  size_t n = 0;
 
   printf("%s\n",prompt);
+  getline(&lineptr, &n, stdin);
+
+  free(lineptr);
   
   return 0;
 }
