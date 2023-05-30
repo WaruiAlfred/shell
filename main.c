df260ffd2ce7759499154a253774ae2503efdbfb
WaruiAlfred <alfredwarui17@gmail.com>
2023-05-30 17:39:32 +0300

    declare void variables

--- main.c
+++ main.c
@@ -6,6 +6,9 @@ int main(int ac,char **argv){
   char *lineptr;
   size_t n = 0;
 
+  /* declaring void variables */
+  (void)ac; (void)argv;
+
   printf("%s\n",prompt);
   getline(&lineptr, &n, stdin);
   printf("%s\n", lineptr);
