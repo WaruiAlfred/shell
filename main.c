6f1b72b8a83f1a502191ecee72b88ffb0e18b7f2
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-01 17:06:57 +0300

    add code for exiting shell

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main(int ac,char **argv){
   char *prompt = "(shell) $";
   char *lineptr;
   size_t n = 0;
+  ssize_t nchars_read;
 
   /* declaring void variables */
   (void)ac; (void)argv;
@@ -11,10 +12,18 @@ int main(int ac,char **argv){
   while(1){
     printf("%s ",prompt);
     lineptr = NULL;
-    getline(&lineptr, &n, stdin);
+    nchars_read =  getline(&lineptr, &n, stdin);
+
+    /* check if the getline function failed or reached EOF or user use CTRL + D */ 
+    if (nchars_read == -1){
+        // printf("Exiting shell....\n");
+        return (-1);
+    }
+
     printf("%s\n", lineptr);
 
     free(lineptr);
   }
+
   return 0;
 }
