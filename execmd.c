876f582491b7d0660a608f5515189ebb99adb993
WaruiAlfred <alfredwarui17@gmail.com>
2023-06-07 17:29:44 +0300

    add get location function for entered commands

--- execmd.c
+++ execmd.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 void execmd(char **argv){
-    char *command = NULL;
+    char *command = NULL, *actual_command = NULL;
 
     if (argv){
         /* get the command */
         command = argv[0];
 
+        actual_command = get_location(command);
+
         /* execute the command with execve */
-        if (execve(command, argv, NULL) == -1){
+        if (execve(actual_command, argv, NULL) == -1){
             perror("Error:");
         };
     }
